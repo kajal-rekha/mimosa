602c660469da1db0e8f8d494a71e92e9b063f416
kajal-2011 <kajalrekha888@gmail.com>
2023-08-19 18:32:12 +0600

    navbar added

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -2,32 +2,52 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <header className="h-20 border-b border-grey bg-light flex">
-      <div>
+    <header className="h-20 border-b border-grey bg-white flex items-center">
+      <div className="wrapper grid grid-cols-3 gap-20 items-center">
         {/* NAV LEFT */}
         <nav>
-          <ul>
+          <ul className="flex gap-5">
             <li>
-              <Link href="/">Link 1</Link>
-              <Link href="/">Link 2</Link>
-              <Link href="/">Link 3</Link>
+              <Link href="/" className="link-item">
+                Link 3
+              </Link>
+            </li>
+            <li>
+              <Link href="/" className="link-item">
+                Link 1
+              </Link>
+            </li>
+            <li>
+              <Link href="/" className="link-item">
+                Link 2
+              </Link>
             </li>
           </ul>
         </nav>
         {/* NAV MID */}
         <nav>
-          <ul>
+          <ul className="flex justify-center">
             <li>
-              <Link href="/">mimosa</Link>
+              <Link href="/" className="lowercase text-4xl font-bold text-pink">
+                mimosa
+              </Link>
             </li>
           </ul>
         </nav>
         {/* NAV RIGHT */}
         <nav>
-          <ul>
+          <ul className="flex gap-5 justify-end">
             <li>
-              <Link href="/">Link 4</Link>
-              <Link href="/">Link 5</Link>
+              <Link href="/" className="link-item">
+                Link 4
+              </Link>
+            </li>
+            <li>
+              <Link href="/" className="link-item">
+                Link 5
+              </Link>
+            </li>
+            <li>
               <Link href="/user/sign-in">Sign in</Link>
             </li>
           </ul>
