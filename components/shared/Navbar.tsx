d0131647146374a6564d92828279d9c482fd15eb
kajal-2011 <kajalrekha888@gmail.com>
2023-08-19 13:41:33 +0600

    done

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,5 +1,40 @@
+import Link from "next/link";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="h-20 border-b border-grey bg-light flex">
+      <div>
+        {/* NAV LEFT */}
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">Link 1</Link>
+              <Link href="/">Link 2</Link>
+              <Link href="/">Link 3</Link>
+            </li>
+          </ul>
+        </nav>
+        {/* NAV MID */}
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">mimosa</Link>
+            </li>
+          </ul>
+        </nav>
+        {/* NAV RIGHT */}
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">Link 4</Link>
+              <Link href="/">Link 5</Link>
+              <Link href="/user/sign-in">Sign in</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colore: {
+      colors: {
         dark: "#19191B",
         light: "#FFFCFA",
         grey: "#E2E0DF",
