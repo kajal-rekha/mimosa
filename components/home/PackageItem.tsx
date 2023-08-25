4624b474988d8dfc08f56ef991bdcdf4308c4fdc
kajal-2011 <kajalrekha888@gmail.com>
2023-08-25 16:02:18 +0600

    packages cards added

---
+++ components/home/PackageItem.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { buttonVariants } from '../ui/Button';
+import { formatCurrency } from '@/utils/formatCurrenct';
+
+interface PackageItemProps {
+  id: number;
+  title: string;
+  image: string;
+  category: string;
+  description: string;
+  price: number;
+}
+
+const PackageItem: React.FC<PackageItemProps> = ({
+  id,
+  title,
+  image,
+  category,
+  description,
+  price,
+}) => {
+  return (
+    <div className='flex w-full flex-col gap-5'>
+      <Link
+        href={`/packages/${id}`}
+        className='group h-[15rem] w-full overflow-hidden rounded-xl border border-grey'
+      >
+        <Image
+          src={image}
+          alt={title}
+          height={640}
+          width={640}
+          priority
+          className='brigtness-90 group-hover:brigtness-100 eq h-full w-full object-cover group-hover:scale-125'
+        />
+      </Link>
+
+      <div className='eq flex w-full flex-col gap-2.5 rounded-xl border bg-grey/25 p-5 hover:bg-grey/50 '>
+        <span className='text-sm font-bold uppercase tracking-[0.375em] text-green'>
+          {category}
+        </span>
+        <h4 className='text-2xl'>{title}</h4>
+        <p className=''>{description}</p>
+        <div className='flex items-center justify-between gap-2.5'>
+          <p className='text-2xl'>{formatCurrency(price)}</p>
+          <Link href={`/packages/${id}`} className={buttonVariants()}>
+            View details
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PackageItem;
--- components/home/Packages.tsx
+++ components/home/Packages.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import { data } from '@/data/packages';
 import HorizontalTab from '../shared/HorizontalTab';
 import SectionTitle from '../shared/SectionTitle';
+import PackageItem from './PackageItem';
 
 const Packages = () => {
   return (
     <section className='wrapper mt-20' id='packages'>
       <SectionTitle title='Explore our beauty packages' subTitle='Packages' />
       <HorizontalTab tabs={['Skincare', 'Haircare', 'Makeup Artistry']}>
-        <p>Tab 1 Content</p>
-        <p>Tab 2 Content</p>
-        <p>Tab 3 Content</p>
+        {/* SKIN CARE */}
+        <div className='grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+          {data
+            .filter((item: any) => item.masterCategory === 'Skincare')
+            .map((item: any) => (
+              <PackageItem key={item.id} {...item} />
+            ))}
+        </div>
+        {/* Haircare */}
+        <div className='grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+          {data
+            .filter((item: any) => item.masterCategory === 'Haircare')
+            .map((item: any) => (
+              <PackageItem key={item.id} {...item} />
+            ))}
+        </div>
+        {/* Makeup Artistry */}
+        <div className='grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+          {data
+            .filter((item: any) => item.masterCategory === 'Makeup Artistry')
+            .map((item: any) => (
+              <PackageItem key={item.id} {...item} />
+            ))}
+        </div>
       </HorizontalTab>
     </section>
   );
