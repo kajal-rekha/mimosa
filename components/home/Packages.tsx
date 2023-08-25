bebb1226f03f7b8ac987266620e4a26e8aaa09da
kajal-2011 <kajalrekha888@gmail.com>
2023-08-25 11:12:18 +0600

    packages btn added

--- components/home/Packages.tsx
+++ components/home/Packages.tsx
@@ -6,7 +6,7 @@ const Packages = () => {
   return (
     <section className='wrapper mt-20' id='packages'>
       <SectionTitle title='Explore our beauty packages' subTitle='Packages' />
-      <HorizontalTab tabs={['Tab 1', 'Tab 2', 'Tab 3']}>
+      <HorizontalTab tabs={['Skincare', 'Haircare', 'Makeup Artistry']}>
         <p>Tab 1 Content</p>
         <p>Tab 2 Content</p>
         <p>Tab 3 Content</p>
