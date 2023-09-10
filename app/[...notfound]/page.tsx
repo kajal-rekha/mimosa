eda03923bf93a97d8af561087c51739ad8b7fdcb
kajal-2011 <kajalrekha888@gmail.com>
2023-09-11 00:44:03 +0600

    not found page added

--- app/[...notfound]/page.tsx
+++ app/[...notfound]/page.tsx
@@ -1,5 +1,22 @@
+import { buttonVariants } from '@/components/ui/Button';
+import Link from 'next/link';
+
 const NotFoundPage = () => {
-  return <main>NotFoundPage</main>;
+  return (
+    <section className='wrapper section-padding mt-20 text-center'>
+      <p className='mb-4 text-9xl font-bold tracking-widest text-dark'>
+        4<span className='text-red'>0</span>4
+      </p>
+      <p className='text-2xl font-semibold text-dark/75'>
+        Uh-oh! The requested page seems to be missing.
+      </p>
+      <div className='mt-8'>
+        <Link href='/' className={buttonVariants({ variant: 'violet' })}>
+          Go to home
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default NotFoundPage;
