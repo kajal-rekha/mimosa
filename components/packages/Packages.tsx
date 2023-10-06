42bf78f390acbaa37fc5fa1cf5c6e8c834bf09cb
kajal-2011 <kajalrekha888@gmail.com>
2023-10-07 02:37:28 +0600

    swr added

--- components/packages/Packages.tsx
+++ components/packages/Packages.tsx
@@ -1,81 +1,133 @@
-import { data } from '@/data/packages';
+'use client';
+
 import { cn } from '@/lib/utils';
-import { packageItem } from '@/types/packageItem';
-import SectionTitle from '../shared/SectionTitle';
+import { beautyPackageType } from '@/types/beautyPackage';
 import { buttonVariants } from '../ui/Button';
 import HorizontalTab from '../ui/HarizontalTab';
+import SectionTitle from '../shared/SectionTitle';
 import PackageItem from './PackageItem';
 import Link from 'next/link';
+import useFetch from '@/hooks/use-fetch';
+import Loading from '../shared/loading';
+import Error from '../shared/error';
 
 interface PackagesProps {
   fromPackagePage?: boolean;
 }
-const Packages: React.FC<PackagesProps> = ({ fromPackagePage }) => {
-  return (
-    <section className='wrapper mt-64' id='packages'>
-      <SectionTitle title='Explore our beauty packages' subTitle='Packages' />
-      <HorizontalTab tabs={['Skincare', 'Haircare', 'Makeup Artistry']}>
-        {/* SKIN CARE */}
-        <div className='grid grid-cols-1 gap-10 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
-          {!fromPackagePage &&
-            data
-              .filter((item: packageItem) => item.masterCategory === 'Skincare')
-              .sort((a: packageItem, b: packageItem) => a.price - b.price)
-              .slice(0, 4)
-              .map((item: packageItem) => (
-                <PackageItem key={item.id} packageItem={item} />
-              ))}
-          {fromPackagePage &&
-            data
-              .filter((item: packageItem) => item.masterCategory === 'Skincare')
-              .sort((a: packageItem, b: packageItem) => a.price - b.price)
-              .map((item: packageItem) => (
-                <PackageItem key={item.id} packageItem={item} />
-              ))}
-        </div>
-        {/* Haircare */}
-        <div className='grid grid-cols-1 gap-10 md:grid-cols-2  xl:grid-cols-3 2xl:grid-cols-4'>
-          {!fromPackagePage &&
-            data
-              .filter((item: packageItem) => item.masterCategory === 'Haircare')
-              .sort((a: packageItem, b: packageItem) => a.price - b.price)
-              .slice(0, 4)
-              .map((item: packageItem) => (
-                <PackageItem key={item.id} packageItem={item} />
-              ))}
-          {fromPackagePage &&
-            data
-              .filter((item: packageItem) => item.masterCategory === 'Haircare')
-              .sort((a: packageItem, b: packageItem) => a.price - b.price)
-              .map((item: packageItem) => (
-                <PackageItem key={item.id} packageItem={item} />
-              ))}
-        </div>
-        {/* Makeup Artistry */}
-        <div className='grid grid-cols-1 gap-10 md:grid-cols-2  xl:grid-cols-3 2xl:grid-cols-4'>
-          {!fromPackagePage &&
-            data
-              .filter(
-                (item: packageItem) => item.masterCategory === 'Makeup Artistry'
-              )
-              .sort((a: packageItem, b: packageItem) => a.price - b.price)
-              .slice(0, 4)
-              .map((item: packageItem) => (
-                <PackageItem key={item.id} packageItem={item} />
-              ))}
-          {fromPackagePage &&
-            data
-              .filter(
-                (item: packageItem) => item.masterCategory === 'Makeup Artistry'
-              )
-              .sort((a: packageItem, b: packageItem) => a.price - b.price)
-              .map((item: packageItem) => (
-                <PackageItem key={item.id} packageItem={item} />
-              ))}
-        </div>
-      </HorizontalTab>
 
-      {!fromPackagePage && (
+const Packages: React.FC<PackagesProps> = ({ fromPackagePage }) => {
+  const {
+    data: beautyPackages,
+    error,
+    isLoading,
+  } = useFetch('/api/beauty_packages');
+
+  return (
+    <section className='wrapper section-padding mt-64' id='packages'>
+      <SectionTitle title='Explore our beauty packages' subTitle='Packages' />
+
+      {isLoading && <Loading isLoading={isLoading} />}
+
+      {error && <Error error={error.message} />}
+
+      {/* PACKAGES */}
+      {beautyPackages && beautyPackages.length > 0 && (
+        <HorizontalTab tabs={['Skincare', 'Haircare', 'Makeup Artistry']}>
+          {/* Skin Care */}
+          <div className='grid grid-cols-1 gap-10 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
+            {!fromPackagePage &&
+              beautyPackages
+                .filter(
+                  (item: beautyPackageType) => item.category === 'Skincare'
+                )
+                .sort(
+                  (a: beautyPackageType, b: beautyPackageType) =>
+                    a.price - b.price
+                )
+                .slice(0, 4)
+                .map((item: beautyPackageType) => (
+                  <PackageItem key={item._id} packageItem={item} />
+                ))}
+
+            {fromPackagePage &&
+              beautyPackages
+                .filter(
+                  (item: beautyPackageType) => item.category === 'Skincare'
+                )
+                .sort(
+                  (a: beautyPackageType, b: beautyPackageType) =>
+                    a.price - b.price
+                )
+                .map((item: beautyPackageType) => (
+                  <PackageItem key={item._id} packageItem={item} />
+                ))}
+          </div>
+
+          {/* Haircare */}
+          <div className='grid grid-cols-1 gap-10 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
+            {!fromPackagePage &&
+              beautyPackages
+                .filter(
+                  (item: beautyPackageType) => item.category === 'Haircare'
+                )
+                .sort(
+                  (a: beautyPackageType, b: beautyPackageType) =>
+                    a.price - b.price
+                )
+                .slice(0, 4)
+                .map((item: beautyPackageType) => (
+                  <PackageItem key={item._id} packageItem={item} />
+                ))}
+
+            {fromPackagePage &&
+              beautyPackages
+                .filter(
+                  (item: beautyPackageType) => item.category === 'Haircare'
+                )
+                .sort(
+                  (a: beautyPackageType, b: beautyPackageType) =>
+                    a.price - b.price
+                )
+                .map((item: beautyPackageType) => (
+                  <PackageItem key={item._id} packageItem={item} />
+                ))}
+          </div>
+
+          {/* Makeup Artistry */}
+          <div className='grid grid-cols-1 gap-10 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
+            {!fromPackagePage &&
+              beautyPackages
+                .filter(
+                  (item: beautyPackageType) =>
+                    item.category === 'Makeup Artistry'
+                )
+                .sort(
+                  (a: beautyPackageType, b: beautyPackageType) =>
+                    a.price - b.price
+                )
+                .slice(0, 4)
+                .map((item: beautyPackageType) => (
+                  <PackageItem key={item._id} packageItem={item} />
+                ))}
+
+            {fromPackagePage &&
+              beautyPackages
+                .filter(
+                  (item: beautyPackageType) =>
+                    item.category === 'Makeup Artistry'
+                )
+                .sort(
+                  (a: beautyPackageType, b: beautyPackageType) =>
+                    a.price - b.price
+                )
+                .map((item: beautyPackageType) => (
+                  <PackageItem key={item._id} packageItem={item} />
+                ))}
+          </div>
+        </HorizontalTab>
+      )}
+
+      {!fromPackagePage && beautyPackages && beautyPackages.length > 0 && (
         <div className='mt-20 flex justify-center'>
           <Link
             href='/packages'
---
+++ components/shared/loading.tsx
@@ -0,0 +1,21 @@
+import { BeatLoader } from 'react-spinners';
+
+interface LoadingProps {
+  isLoading: boolean;
+}
+
+const Loading: React.FC<LoadingProps> = ({ isLoading }) => {
+  return (
+    <div className='flex justify-center'>
+      <BeatLoader
+        color='#32B8CB'
+        loading={isLoading}
+        size={24}
+        data-testid='loader'
+        aria-label='Loading Spinner'
+      />
+    </div>
+  );
+};
+
+export default Loading;
